5dd81d503528a36f775145c9c00a5e1edfbfffe3
Daniel Felipe <felipealvisbermudez@gmail.com>
2026-03-02 23:46:57 -0500

    Creacion Apps Users e Stories

---
+++ Users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    embedding = models.JSONField(null=True, blank=True)
+    reconocimiento_facial = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
